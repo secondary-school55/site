15be4500e3c944af273f60feb43bad8d020006d0
secondary-school55 <kzh55@ukr.net>
2020-10-12 10:12:48 +0300

    update

--- components/posts/thumbnail.js
+++ components/posts/thumbnail.js
@@ -1,11 +1,11 @@
 export default function Thumbnail(props) {
   let { src } = props;
 
-  return (
-    <img
-      {...props}
-      src={src}
-      onError={(ev) => (ev.target.src = "/no-thumbnail.svg")}
-    />
-  );
+  function onError(ev) {
+    if (ev.target.src.endsWith(".jpg"))
+      ev.target.src = ev.target.src.replace(".jpg", ".webp");
+    else ev.target.src = "/no-thumbnail.svg";
+  }
+
+  return <img {...props} src={src} onError={onError} />;
 }
