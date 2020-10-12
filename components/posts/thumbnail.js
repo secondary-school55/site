9195b561c92c7b4c64a86147ab5aca8499be9a38
secondary-school55 <kzh55@ukr.net>
2020-10-12 10:28:06 +0300

    update

--- components/posts/thumbnail.js
+++ components/posts/thumbnail.js
@@ -2,9 +2,7 @@ export default function Thumbnail(props) {
   let { src } = props;
 
   function onError(ev) {
-    if (ev.target.src.endsWith(".jpg"))
-      ev.target.src = ev.target.src.replace(".jpg", ".webp");
-    else ev.target.src = "/no-thumbnail.svg";
+    ev.target.src = "/no-thumbnail.svg";
   }
 
   return <img {...props} src={src} onError={onError} />;
