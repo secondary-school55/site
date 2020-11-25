95ffc83ad35919d317a5a2432e5b3306ca7bb691
secondary-school55 <kzh55@ukr.net>
2020-11-25 09:13:03 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,21 @@ import "styles/global.css";
 import "react-sortable-tree/style.css";
 import "flickity/dist/flickity.min.css";
 import "react-table/react-table.css";
-
+import { SWRConfig } from "swr";
 import Layout from "components/layout";
 import { useRouterScroll } from "lib/scroll";
 
 export default function App({ Component, pageProps }) {
   useRouterScroll();
   return (
-    <Layout posts={pageProps.posts}>
-      <Component {...pageProps} />
-    </Layout>
+    <SWRConfig
+      value={{
+        fetcher: (...a) => fetch(...a).then((r) => r.json()),
+      }}
+    >
+      <Layout posts={pageProps.posts}>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
   );
 }
