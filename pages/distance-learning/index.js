16da00cc37130c8eeea598cf307202b6dab2be3f
secondary-school55 <kzh55@ukr.net>
2020-12-03 10:30:13 +0200

    update

---
+++ pages/distance-learning/index.js
@@ -0,0 +1,118 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-irregular-whitespace */
+
+import Md from "components/md";
+import Pdf from "components/pdf";
+import { Tab, Tabs } from "components/tabs";
+
+export default function DistanceLearning() {
+  return (
+    <Tabs>
+      <Tab title="Карантин. Нормативна база">
+        <Md>{`
+1.  [Щодо організації дістанційного навчання під час карантину](https://drive.google.com/open?id=1OJ5lMUHVveQi3lfCmWCwqdigSoGFovNQ)
+1.  [Щодо організації освітнього процесу в закладах середньої освіти під час карантину](https://drive.google.com/open?id=1ZWDPmnGw2D4j8MWHgtSvWu__l_5LaDnG)
+1.  [Про посилення заходів безпеки щодо запобігання поширенню коронавірусу COVID-19 серед учасників освітнього процесу](https://drive.google.com/open?id=1vgncbyQQqmj_wBwv68y2eqotOSh0r2jh)
+1.  [Про затвердження методичних рекомендацій щодо встановлення гнучкого режиму робочого часу](https://drive.google.com/open?id=1HOHg4VKCOyqkZ6WBT4Z878CJNu8o2EAl)
+1.  [Про попередження виробничого травматизму, дотримання пожежної безпеки під час впровадження карантину](https://drive.google.com/open?id=1wF18E0ORFxjOuuLywWYW9nyzPZtP2Xy_)
+1.  [Про припинення освітнього процесу у закладах освіти міста](https://drive.google.com/open?id=1VCZQqAVFZUrVVcEsuEITYK5PoPzcStwa)
+1.  [Про організаційні заходи для запобігання поширенню коронаврусу COVID-19](https://drive.google.com/open?id=1934WxL6ORUPTVtnAq5LR39JXSDby9uq_)
+1.  [КЗШ №55. Щодо підсумків проведення педагогічної ради в режимі он-лайн 05.05.2020](https://drive.google.com/open?id=1lnZyuw83arjcgVPYOcvhTMOtgol7a1V9)
+1.  [КЗШ №55. Про внесення змін до наказу від 26.03.2020 № 80](https://drive.google.com/open?id=1WdM9hlwzAn0PbQSK6hjBOkolF8zpO8L5)
+1.  [КЗШ №55. Про зміни до наказу від 12.03.2020 № 70 «Про припинення освітнього процесу у КЗШ № 55 КМР ДО»](https://drive.google.com/open?id=14ytFnV5WE1uhneCdCBOeap25PtfAfbDn)
+1.  [КЗШ №55. Про зміни до наказу від 24.04.2020 № 93 «Про припинення освітнього процесу у КЗШ № 55 КМР ДО»](https://drive.google.com/open?id=1tjVvkXrHKYea3B36crwGNRg2ThKvu56k)
+1.  [КЗШ №55. Про припинення освітнього процесу у КЗШ № 55 КМР ДО](https://drive.google.com/open?id=1ZvFdsbJlbDqmSBwJtIUk-cbLFxoZDin0)
+1.  [КЗШ №55. Про посилення обмежувальних заходів щодо попередження коронавірусу COVID-19](https://drive.google.com/open?id=19umBcxvtDJzIq2oHf8QOHqowhHtQjD89)
+1.  [КЗШ №55. Про організацію роботи у період карантину](https://drive.google.com/open?id=1Zt4emdrBndvi97EuQbsxMcpa6hQ-F0vA)
+1.  [КЗШ №55. Про попередження виробничого травматизму, дотримання пожежної безпеки під час впровадження карантину](https://drive.google.com/open?id=1irhf0sCZ53aZxLRZNgigFba1xa4JS85x)
+1.  [КЗШ №55. Про посилення обмежувальних заходів щодо попередження розповсюдження коронавірусу COVID – 19](https://drive.google.com/open?id=1EZ3Yb-kpKZYLfYGb2p48u6GTOeoyHIzC)
+1.  [КЗШ №55. Про припинення освітнього процесу у КЗШ № 55 КМР ДО](https://drive.google.com/open?id=1nk_UyMHAUVHDpVSqhI_n3zfRGP3-WBF7)
+1.  [КЗШ №55. Щодо організації освітнього процесу в закладі освіти під час карантину](https://drive.google.com/open?id=1P4sQSohSlPALa6_sUXeCeM_P302JA4Xc)
+1.  [КЗШ №55. Щодо організації дистанційного навчання в освітньому закладі під час карантину](https://drive.google.com/open?id=16ff8k1lHQ-MKmHbyOv07o5cbyZaOa3FD)
+`}</Md>
+      </Tab>
+      <Tab title="1 клас">
+        <Md>{`
+`}</Md>
+      </Tab>
+      <Tab title="2 клас">
+        <Md>{`
+`}</Md>
+      </Tab>
+      <Tab title="3 клас">
+        <Md>{`
+** Музичне мистецтво **  
+1.  **03.12** [Тема: Найдорожча у світі](https://drive.google.com/file/d/1i1gUjCiN0vKPL8XrNsCkVmbBWdbUVxog/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="4 клас">
+        <Md>{`
+`}</Md>
+      </Tab>
+      <Tab title="5 клас">
+        <Md>{`
+** Фізика **  
+1.  **03.12** [Тема: Поняття про сузір’я. Зорі.](https://drive.google.com/file/d/1R7UCQIPSK5QBTiTtfQ6qKkuGD4RD3D5G/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="6 клас">
+        <Md>{`
+`}</Md>
+      </Tab>
+      <Tab title="7 клас">
+        <Md>{`
+** Фізика **  
+1.  **03.12** [Тема: Коливальний рух. Амплітуда коливань. Період і частота коливань. Маятники.](https://drive.google.com/file/d/1qvwaKnFReAwoJV2QDkk4CS5k-MpwJtrd/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="8 клас">
+        <Md>{`
+** Українська мова **  
+1.  **03.12** [Тема: Односкладні прості речення з одним головним членом підметом. Називні речення](https://drive.google.com/file/d/1VuUQRDDL4JHv2u27c3eGMZbOwm5faErV/view?usp=sharing)
+
+** Хімія **  
+1.  **03.12** [Тема: Кількість речовини. Моль – одиниця кількості речовини. Число Авогадро.](https://drive.google.com/file/d/1Xza-GsZgxJa88JxRQB42fENhbGkA2hPj/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="9 клас">
+        <Md>{`
+** Географія **  
+1.  **03.12** [Тема: Видобування металічних руд](https://drive.google.com/file/d/1nLsYYlfSAhh_c-YzVUL8iJaKDekY1NXc/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="10 клас">
+        <Md>{`
+** Українська мова **  
+1.  **03.12** [Тема: Лексичні норми української мови](https://drive.google.com/file/d/1XT-saYAg8GwSe8t6S7x7aldPogbaVAMc/view?usp=sharing)
+
+** Фізика **  
+1.  **03.12** [Тема: Механічна робота. Потужність. Кінетична енергія.](https://drive.google.com/file/d/13ktDbpkU-YGMFOs3KRcYIqKot5LfswUQ/view?usp=sharing)
+`}</Md>
+      </Tab>
+      <Tab title="11 клас">
+        <Md>{`
+** Українська література **  
+1.  **03.12** [Тема: «Патетична соната» - драматична опозиція високої мрії і потворної дійсності](https://drive.google.com/file/d/1-l7D_zFg1QA_-aGbH1lADDCULEBgChgc/view?usp=sharing)  
+1.  **03.12** [Тема: Микола Гурович Куліш](https://drive.google.com/file/d/1FMV2UWTFbPSAjYr4Gwhv4lIHsEnHucaA/view?usp=sharing)`}</Md>
+      </Tab>
+      <Tab title="Онлайн уроки">
+        <Md>{`
+1 клас –  [bit.ly/USO1class](https://bit.ly/USO1class)  
+2 клас –  [bit.ly/USO2class](https://bit.ly/USO2class)  
+3 клас –  [bit.ly/USO3class](https://bit.ly/USO3class)  
+4 клас –  [bit.ly/USO4class](https://bit.ly/USO4class)  
+5 клас –  [bit.ly/USO5class](https://bit.ly/USO5class)  
+6 клас –  [bit.ly/USO6class](https://bit.ly/USO6class)  
+7 клас –  [bit.ly/USO7class](https://bit.ly/USO7class)  
+8 клас –  [bit.ly/USO8class](https://bit.ly/USO8class)  
+9 клас –  [bit.ly/USO9class](https://bit.ly/USO9class)  
+10 клас – [bit.ly/USO10class](https://bit.ly/USO10class)  
+11 клас – [bit.ly/USO11class](https://bit.ly/USO11class)  
+`}</Md>
+      </Tab>
+      <Tab title="Зворотній зв'язок">
+        <Pdf src="/distance-learning/emails.pdf" height="1200px" />
+      </Tab>
+    </Tabs>
+  );
+}
