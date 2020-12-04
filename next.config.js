a7f52d17dc7edf5b4cdc7f3772cd0bac316adbcc
secondary-school55 <kzh55@ukr.net>
2020-12-04 15:45:02 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
-const withMdx = require("@next/mdx")({ extension: /\.mdx?$/ });
 const withYaml = require("next-plugin-yaml");
 const withXlsx = require("./plugins/next-plugin-xlsx");
 
 module.exports = withYaml(
-  withMdx(
-    withXlsx({
-      pageExtensions: ["js", "mdx"],
-      webpack: (config) => {
-        config.module.rules.push({
-          test: require.resolve(`${__dirname}/lib/posts`),
-          use: [{ loader: "val-loader" }],
-        });
-        return config;
-      },
-    })
-  )
+  withXlsx({
+    webpack: (config) => {
+      config.module.rules.push({
+        test: require.resolve(`${__dirname}/lib/posts`),
+        use: [{ loader: "val-loader" }],
+      });
+      return config;
+    },
+  })
 );
