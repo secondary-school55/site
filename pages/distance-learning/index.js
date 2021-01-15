bcfd68dd574e7d7aa19955e65e480a5c2b27a3af
secondary-school55 <kzh55@ukr.net>
2021-01-15 10:55:26 +0200

    update

--- pages/distance-learning/index.js
+++ pages/distance-learning/index.js
@@ -847,6 +847,7 @@ export default function DistanceLearning() {
 
 ** Українська література **  
 **13.01** [7-А,Б. Тема: Григір Тютюнник. Коротко про дитинство письменника. «Климко» - автобіографічна основа твору](https://drive.google.com/file/d/1FVGnicieGXVOTeJ_XWiD9Y-VfmPokNuN/view?usp=sharing)  
+**15.01** [7-А,Б. Тема: Григір Тютюнник «Климко».  Морально-етичні уроки доброти, чуйності, турботи про рідних»](https://drive.google.com/file/d/12JoKMVq3rxqTtfvIPPe39SJ_zpH8tT5i/view?usp=sharing)  
 `}</Md>
       </Tab>
       <Tab title="8 клас">
